495ef89660a337059d732b04fb6c4c0e21349ffc
JoaoVitorSantDrade <jvitorsantdrade@gmail.com>
2022-12-06 00:34:45 -0300

    ARP update
    Busca por pares na rede feita
    
    Falta:
    Enviar mensagens p/ pares na rede

--- peer.py
+++ peer.py
@@ -16,62 +16,93 @@ class Peer_Type(Enum):
     RECIEVER = 2
 
 class Peer:
-    def __init__(self, type:Peer_Type):
+    def __init__(self, type:Peer_Type,host:str, port:int):
         self.id:uuid = uuid.uuid4()
-        self.connection:list = list()
+        self.connection:set = set()
+        self.SuccefullConnection:dict = dict()
         self.type:Peer_Type = type
+        self.Host:str = host
+        self.Port:int = port
 
     def Add_connection(self, url:str):
-        self.connection.append(url)
+        self.connection.add(url)
 
     def Del_connection(self, url:str):
-        self.connection.remove(url)
+        try:
+            self.connection.remove(url)
+        except Exception:
+            print("URL não cadastrada")
 
-    def Try_send(self, item:dict):
+    def ARP(self):
         try:
             assert self.type == Peer_Type.SENDER
 
+            retry = Retry(backoff_factor=0.02, connect=2)
             session = requests.Session()
-            retry = Retry(connect=1, backoff_factor=1)
             adapter = HTTPAdapter(max_retries=retry)
             session.mount('http://', adapter)
             session.mount('https://', adapter)
-
+            headers = {'content-type': 'application/json'}
+        except Exception as exp:
+            print(exp)
+        try:
             for url in self.connection:
                 for i in conf.PORT_RANGE:
-                    url = url + ":" + str(i)
-                    print(url)
-                    r = session.post(url, data="test".encode())
-                    print(r.status_code)
-                    print(r.headers)
+                    if i != self.Port:
+                        try:
+                            link = url + ":" + str(i)
+                            r = session.post(link + "/api/connection" , data=self.Host, headers=headers, timeout=0.002) #Adiciona par encontrado na lista
+                            print(f"Par encontrado em: {link} - {r.status_code}")
+                            self.SuccefullConnection.update({url:str(i)})
+                        except requests.exceptions.ConnectionError as errc:
+                            print(f"Falha ao conectar em :{i}")
+                            pass
+                        except requests.exceptions.Timeout as errt:
+                            print("Timeout")
+                            pass
+                        except requests.exceptions.InvalidURL as erriu:
+                            print("Invalid URL")
+                            pass
+                        except AssertionError as erae:
+                            print("Peer type is incorrect")
+                            pass
+                        except Exception as exp:
+                            print(exp)
+                            pass
+                    
+        except Exception as MainFailure:
+            print(MainFailure)
 
-        except requests.exceptions.ConnectionError as errc:
-            print(errc)
-        except requests.exceptions.Timeout as errt:
-            print(errt)
-        except requests.exceptions.InvalidURL as erriu:
-            print(erriu)
-        except AssertionError as erae:
-            print("Peer type is incorrect")
-        except Exception as exp:
-            pass
+    def sendMessage(self, msg, id):
+        pass
+
+Main_Peer:Peer = None
 
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
-def index():
-    print(request.data)
+
+@app.route('/api/connection', methods=['POST'])
+def arp():
+    args = request.data.decode() #Caso seja encontrado por um par
+    print(f'Foi encontrado isso: {args}')
+    Main_Peer.Add_connection(args)
+    return "oi"
 
 
-if __name__ == '__main__':
-    par = Peer(Peer_Type.SENDER)
-    par.Add_connection("https://localhost")
-    par.Add_connection("https://google.com")
-    msg = {"id": "01","quantity": "5","name": "GPU","marketplace": "Americanas","price": "1000"}
+if __name__ == '__main__': #Teste
+
+    IP = input("Digite o IP: ")
+    port = int(input("Digite a Porta: "))
+    par = Peer(Peer_Type.SENDER, host=IP, port=port)
+    Main_Peer = par
+    par.Add_connection("http://localhost")
     try:
-        sender = Thread(target=par.Try_send, args=(msg,))
-        receiver = Thread(target=app.run, args=("0.0.0.0",10250,))
+        sender = Thread(target=par.ARP)
+        receiver = Thread(target=app.run, args=("0.0.0.0",port,))
 
         sender.start()
         receiver.start()
     except Exception as expt:
         print(expt)
+    finally: 
+        receiver.join()
+        sender.join()
