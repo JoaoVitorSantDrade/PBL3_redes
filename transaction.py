426404a3e15738971e20dc2c495e6f293bf8e9d7
ALrEcompUefs <contaofcali@gmail.com>
2022-12-07 20:46:11 -0300

    criando transacao

--- transaction.py
+++ transaction.py
@@ -1,79 +1,16 @@
-from __future__ import annotations
-from abc import ABC, abstractmethod
+import uuid
+from vector import vector_clock
+class transaction:
+    def __init__(self,clock:vector_clock):
+        self.id:uuid = uuid.uuid4()
+        self.clock:vector_clock = clock
+        self.itens:dict = dict()
+        self.status:int = 0
+    
+    def status_change(self,status:int):
+        self.status=status
 
-class Context:
-
-    _state = None
-
-    def __init__(self, state: State) -> None:
-        self.transition_to(state)
-
-    def transition_to(self, state: State):
-
-        print(f"Context: Transition to {type(state).__name__}")
-        self._state = state
-        self._state.context = self
-
-    def request1(self):
-        self._state.handle1()
-
-    def request2(self):
-        self._state.handle2()
-
-class State(ABC):
-    """
-    The base State class declares methods that all Concrete State should
-    implement and also provides a backreference to the Context object,
-    associated with the State. This backreference can be used by States to
-    transition the Context to another State.
-    """
-
-    @property
-    def context(self) -> Context:
-        return self._context
-
-    @context.setter
-    def context(self, context: Context) -> None:
-        self._context = context
-
-    @abstractmethod
-    def handle1(self) -> None:
-        pass
-
-    @abstractmethod
-    def handle2(self) -> None:
-        pass
-
-
-"""
-Concrete States implement various behaviors, associated with a state of the
-Context.
-"""
-
-
-class ConcreteStateA(State):
-    def handle1(self) -> None:
-        print("ConcreteStateA handles request1.")
-        print("ConcreteStateA wants to change the state of the context.")
-        self.context.transition_to(ConcreteStateB())
-
-    def handle2(self) -> None:
-        print("ConcreteStateA handles request2.")
-
-
-class ConcreteStateB(State):
-    def handle1(self) -> None:
-        print("ConcreteStateB handles request1.")
-
-    def handle2(self) -> None:
-        print("ConcreteStateB handles request2.")
-        print("ConcreteStateB wants to change the state of the context.")
-        self.context.transition_to(ConcreteStateA())
-
-
-if __name__ == "__main__":
-    # The client code.
-
-    context = Context(ConcreteStateA())
-    context.request1()
-    context.request2()
+    def transaction_kill(self):
+        del self
+    
+    
